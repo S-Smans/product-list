22e19a77e82d8b882a20d8fc586cfda35c8b1a18
Sandis <smanssandis@gmail.com>
2022-06-21 18:35:47 +0300

    refactor type.js

--- js/type.js
+++ js/type.js
@@ -1,50 +1,44 @@
 // For each option selected create a desired form
 const selection = document.getElementById("productType");
 selection.addEventListener("change", (event) => {
-	toggleForm(event.target.value);
+  toggleForm(event.target.value); // value = book, furniture or dvd
 });
 
-// Picks the correct form layout function
-const productForm= (type) => {
-  // Object that stores each types functions
-  const types = {
-    dvd: setFormVisible("dvdForm"),
-    furniture: setFormVisible("furnitureForm"),
-    book: setFormVisible("bookForm"),
-  };
-
-	// returns function that allows form to become visible
-  return types[type];
-};
-
 const setFormVisible = (type) => {
-	// Get all forms from document
+  // Get all forms from document
   const dvdForm = document.getElementById("dvd-form");
   const furnitureForm = document.getElementById("furniture-form");
   const bookForm = document.getElementById("book-form");
 
-	// Disable all form visibility
+  // Disable all form visibility
   dvdForm.style.display = "none";
-	furnitureForm.style.display = "none";
+  furnitureForm.style.display = "none";
   bookForm.style.display = "none";
-  console.log(bookForm);
-	// Store form visibility toggle in objects
-	const forms = {
-		dvdForm: () => {dvdForm.style.display = "grid"},
-		furnitureForm: () => {furnitureForm.style.display = "grid"},
-		bookForm: () => {bookForm.style.display = "grid"},
-	}
 
-	// Return the form that should become visible
-	return forms[type];
-}
+  // Store form visibility toggle in objects
+  const forms = {
+    dvd: () => {
+      dvdForm.style.display = "grid";
+    },
+    furniture: () => {
+      furnitureForm.style.display = "grid";
+    },
+    book: () => {
+      bookForm.style.display = "grid";
+    },
+  };
+
+  // Return the form that should become visible
+  return forms[type];
+};
 
 // Displays form
 const toggleForm = (type) => {
   // Gets the required form for display
-  const typeForm = productForm(type);
+  const typeForm = setFormVisible(type);
   typeForm();
-}
+};
 
 // Loads form when user enters the page
 toggleForm(selection.value);
+
